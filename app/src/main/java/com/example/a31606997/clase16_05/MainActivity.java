002950c68b55c2e87540ac816f5eaff651d9f449
sebastian <sebad85@hotmail.com>
2019-05-16 20:56:18 -0300

    Se Agrega clase test y comentario en el MainActivity

--- app/src/main/java/com/example/a31606997/clase16_05/MainActivity.java
+++ app/src/main/java/com/example/a31606997/clase16_05/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Cmentario para ver el cambio
+        //Coentario para ver el cambio
         setContentView(R.layout.activity_main);
     }
 }
